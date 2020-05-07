eefd3d16602980fc6216e5f0786981731fea4032
duddn0406 <64262606+duddn0406@users.noreply.github.com>
2020-05-07 21:48:09 +0900

    project.py

---
+++ project.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+
+
+@bot.command()
+async def 봇(ctx):
+    await ctx.send('가동!')
+
+@bot.command()
+async def 청소(ctx, amount=80000000000000000000000000000):
+    await ctx.channel.purge(limit=amount)
+
+@bot.command()
+async def kick (ctx, member : discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+
+
+
+
+bot.run('NzAxNzcwNTI1MjY5MTY0MTAy.XrPSMg.fYSYvzZCFA-IPZEOUrQ-LVWYiJ0 ')
